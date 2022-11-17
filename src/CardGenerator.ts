e21d33eb240afc735aee41adba3cca8114a65a4f
LeBogo <lebogomc@gmail.com>
2022-11-17 17:01:40 +0100

    Improved CardGenerator

--- src/CardGenerator.ts
+++ src/CardGenerator.ts
@@ -1,4 +1,4 @@
-import { Card, CardColor, CardColorType, CardType, GameMode } from "@lebogo/onu2-shared";
+import { Card, CardColor, GameMode } from "@lebogo/onu2-shared";
 
 export class CardGenerator {
     constructor(public mode: GameMode) {}
@@ -7,13 +7,38 @@ export class CardGenerator {
         const cards: Card[] = [];
         const { presets } = this.mode;
         for (let i = 0; i < size; i++) {
-            const { colors, types } = presets[Math.floor(Math.random() * presets.length)];
+            // TODO: Improve this algorithm for efficiency
 
-            const color: CardColorType = colors[Math.floor(Math.random() * colors.length)];
-            const type: CardType = types[Math.floor(Math.random() * types.length)];
+            let presetCards = [];
+            let colors = new Set();
+            for (const preset of presets) {
+                for (const color of preset.colors) {
+                    colors.add(color);
+                    for (const type of preset.types) {
+                        presetCards.push({ color, type });
+                    }
+                }
+            }
 
-            const card = new Card(type, new CardColor(color));
+            // pick a random color
+            const color = [...colors][Math.floor(Math.random() * colors.size)];
+
+            // pick a random card with that color
+            const pickedCard = presetCards
+                .filter((c) => c.color === color)
+                .sort(() => Math.random() - 0.5)[0];
+
+            console.log(pickedCard);
+
+            const card = new Card(pickedCard.type, new CardColor(pickedCard.color));
+
             cards.push(card);
+
+            // const color: CardColorType = colors[Math.floor(Math.random() * colors.length)];
+            // const type: CardType = types[Math.floor(Math.random() * types.length)];
+
+            // const card = new Card(type, new CardColor(color));
+            // cards.push(card);
         }
         return cards;
     }
